eb11947c65f8f4e5e9cfd646cb478527ee233093
Teacher-PE <121098179+Teacher-PE@users.noreply.github.com>
2023-05-26 16:34:09 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -54,4 +54,49 @@ group by
   e.last_name 
 order by 
   s.sale_date;
+  
+  --Решение первой задачи 6 шага
+select 
+	case 
+	when age between 10 and 15 then '10-15' 
+	when age between 16 and 25 then '16-25'
+	when age between 26 and 40 then '26-40'
+	when age > 40 then '40+' end as age_category, 
+ 	count(age) as count 
+from 
+  customers c 
+group by 
+	age_category
+order by 
+  age_category;
+  
+ --Решение второй задачи 6 шага
+select 
+	to_char(sale_date, 'YYYY-MM') as date,
+	count(distinct customer_id) as tota_customers,
+	sum(quantity * price) as income
+from sales s
+left join products p 
+on s.product_id = p.product_id 
+group by 
+	date
+order by 
+	date;
+  
+  --Решение третей задачи 6 шага
+select 
+  distinct on (c.customer_id) concat(c.first_name, ' ', c.last_name) as customer, 
+  sale_date, 
+  concat(e.first_name, ' ', e.last_name) as seller 
+from 
+  sales s 
+  left join employees e on s.sales_person_id = e.employee_id 
+  left join customers c on s.customer_id = c.customer_id 
+  left join products p on s.product_id = p.product_id 
+where 
+  price = 0 
+order by 
+  c.customer_id, 
+  sale_date;
+  
 
