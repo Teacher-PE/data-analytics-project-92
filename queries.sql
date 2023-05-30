4c97c3b21281e87c5068fcac80972b8a8896d132
Teacher-PE <121098179+Teacher-PE@users.noreply.github.com>
2023-05-30 21:49:56 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -21,20 +21,17 @@ order by
   
   --Решение второй задачи 5 шага
 select 
-  concat(e.first_name, ' ', e.last_name) as name, 
-  round(
-    avg(s.quantity * p.price), 
-    0
-  ) as average_income 
-from 
-  sales s 
-  left join employees e on s.sales_person_id = e.employee_id 
-  left join products p on s.product_id = p.product_id 
-group by 
-  e.first_name, 
-  e.last_name 
-order by 
-  average_income asc;
+	concat(e.first_name,' ', e.last_name) as name ,
+	round(avg(s.quantity * p.price),0) as average_income
+from sales s 
+left join employees e 
+on s.sales_person_id = e.employee_id 
+left join products p 
+on s.product_id = p.product_id 
+group by e.first_name, e.last_name
+having avg(s.quantity * p.price) < 267166
+order by average_income asc 
+;
   
   --Решение третей задачи 5 шага
 select 
