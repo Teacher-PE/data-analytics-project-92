77f5cbd2d91123c2020945516baf021412003021
Teacher-PE <121098179+Teacher-PE@users.noreply.github.com>
2023-06-01 11:59:54 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,38 +5,38 @@ from
 
 --Решение первой задачи 5 шага
 select 
-  concat(e.first_name, ' ', e.last_name) as name, 
-  count(s.quantity) as operations, 
-  sum(s.quantity * p.price) as income 
-from 
-  sales s 
-  left join employees e on s.sales_person_id = e.employee_id 
-  left join products p on s.product_id = p.product_id 
-group by 
-  e.first_name, 
-  e.last_name 
-order by 
-  income desc
-  limit 10;
-  
-  --Решение второй задачи 5 шага
+	e.first_name ||' '|| e.last_name as name ,
+	count(s.quantity) as operation,
+	sum(s.quantity * p.price) as income
+from sales s 
+left join employees e 
+on s.sales_person_id = e.employee_id 
+left join products p 
+on s.product_id = p.product_id 
+group by 1
+order by income desc
+limit 10
+;
+
+--Решение второй задачи 5 шага 
 select 
-	concat(e.first_name,' ', e.last_name) as name ,
+	e.first_name ||' '|| e.last_name as name ,
 	round(avg(s.quantity * p.price),0) as average_income
 from sales s 
 left join employees e 
 on s.sales_person_id = e.employee_id 
 left join products p 
 on s.product_id = p.product_id 
-group by e.first_name, e.last_name
+group by 1
 having avg(s.quantity * p.price) < 267166
 order by average_income asc 
 ;
-  
-  --Решение третей задачи 5 шага
+
+--Решение третей задачи 5 шага
 with inc as (
 select 
-concat(e.first_name,' ', e.last_name) as name,extract(isodow from sale_date) as num_day,
+e.first_name ||' '|| e.last_name as name,
+extract(isodow from sale_date) as num_day,
 round(sum(quantity*price),0) as income,
 to_char(sale_date, 'day') as weekday
 from sales s 
@@ -44,12 +44,15 @@ left join employees e
 on s.sales_person_id = e.employee_id 
 left join products p 
 on s.product_id = p.product_id 
-group by e.first_name, e.last_name, extract(isodow from sale_date), to_char(sale_date, 'day')
-order by  extract(isodow from sale_date),e.first_name, e.last_name )
+group by 1, 2, 4
+order by  
+	extract(isodow from sale_date), 
+	name
+)
 select inc.name, inc.weekday, inc.income
 from inc
-  
-  --Решение первой задачи 6 шага
+
+--Решение первой задачи 6 шага
 select 
 	case 
 	when age between 10 and 15 then '10-15' 
@@ -60,37 +63,36 @@ select
 from 
   customers c 
 group by 
-	age_category
+	1
 order by 
   age_category;
-  
- --Решение второй задачи 6 шага
+
+--Решение второй задачи 6 шага
 select 
 	to_char(sale_date, 'YYYY-MM') as date,
-	count(distinct customer_id) as total_customers,
+	count(distinct customer_id) as tota_customers,
 	sum(quantity * price) as income
 from sales s
 left join products p 
 on s.product_id = p.product_id 
 group by 
-	date
+	1
 order by 
 	date;
-  
-  --Решение третей задачи 6 шага
-select 
-  distinct on (c.customer_id) concat(c.first_name, ' ', c.last_name) as customer, 
-  sale_date, 
-  concat(e.first_name, ' ', e.last_name) as seller 
-from 
-  sales s 
-  left join employees e on s.sales_person_id = e.employee_id 
-  left join customers c on s.customer_id = c.customer_id 
-  left join products p on s.product_id = p.product_id 
-where 
-  price = 0 
-order by 
-  c.customer_id, 
-  sale_date;
+
+--Решение третей задачи 6 шага
+select distinct on (c.customer_id) c.first_name ||' '|| c.last_name as customer, 
+sale_date, 
+e.first_name ||' '|| e.last_name as seller
+from sales s
+left join employees e
+on s.sales_person_id = e.employee_id 
+left join customers c 
+on s.customer_id = c.customer_id 
+left join products p
+on s.product_id = p.product_id
+where price = 0
+order by c.customer_id, sale_date;
+
   
 
